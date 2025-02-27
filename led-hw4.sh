eecf117f2baafc7948edfe81b7b8d0af89d3f937
Jeremy <tileratedskink@gmail.com>
2025-02-27 03:16:58 +0000

    commit homework problem

---
+++ led-hw4.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# A small Bash script to set up User LED3 to be turned on or off from 
+#  Linux console. Written by Derek Molloy (derekmolloy.ie) for the 
+#  book Exploring BeagleBone.
+
+LED3_PATH=/sys/class/leds/beaglebone:green:usr3
+
+# Example bash function
+function removeTrigger
+{
+  echo "none" >> "$LED3_PATH/trigger"
+}
+
+echo "Starting the LED Bash Script"
+if [ $# -eq 0 ]; then
+  echo "There are no arguments. Usage is:"
+  echo -e " bashLED Command \n  where command is one of "
+  echo -e "   on, off, flash, status, blink <n>  \n e.g. bashLED on "
+  exit 2
+fi
+echo "The LED Command that was passed is: $1"
+if [ "$1" == "on" ]; then
+  echo "Turning the LED on"
+  removeTrigger
+  echo "1" >> "$LED3_PATH/brightness"
+elif [ "$1" == "off" ]; then
+  echo "Turning the LED off"
+  removeTrigger
+  echo "0" >> "$LED3_PATH/brightness"
+elif [ "$1" == "flash" ]; then
+  echo "Flashing the LED"
+  removeTrigger
+  echo "timer" >> "$LED3_PATH/trigger"
+  sleep 1
+  echo "100" >> "$LED3_PATH/delay_off"
+  echo "100" >> "$LED3_PATH/delay_on"
+elif [ "$1" == "status" ]; then
+  cat "$LED3_PATH/trigger";
+elif [ "$1" == "blink" ]; then
+  if [[ -z "$2" || ! "$2" =~ ^[0-9]+$ ]]; then
+    echo "Error: Please provide a valif number of blinks"
+    exit 3
+  fi
+  echo "Blinking LED $2 times"
+  removeTrigger
+  N=$2
+  for ((i=1; i<=N; i++))
+  do
+   echo "1" >> "$LED3_PATH/brightness"
+   sleep 0.2
+   echo "0" >> "$LED3_PATH/brightness"
+   sleep 0.2
+  done
+fi
+echo "End of the LED Bash Script"
